ec840cd4611041ab162701e0860c4af82872d92f
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-16 02:21:01 +0300

    feat: add constant paths

---
+++ src/shared/constants/PATHS.ts
@@ -0,0 +1,19 @@
+const PATHS = {
+    main: '/main',
+
+    auth: '/auth',
+    registration: '/auth/registration',
+    confirmEmail: '/auth/confirm-email',
+    changePassword: ' /auth/change-password',
+
+    result: '/result',
+    signUpSuccess: '/result/success',
+    changePasswordSuccess: '/result/success-change-password',
+
+    error: '/result/error',
+    logInError: '/result/error-login',
+    emailCheckExistError: '/result/error-check-email-no-exist',
+    userExistError: '/result/error-user-exist',
+    emailCheckError: '/result/error-check-email',
+    changePasswordError: '/result/error-change-password',
+};
