6832e1076e5e237654027f418f98e77be916a361
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-16 02:29:34 +0300

    feat: setup history to store

--- src/redux/configure-store.ts
+++ src/redux/configure-store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history';
+import { createReduxHistoryContext } from 'redux-first-history';
+
+const { createReduxHistory } = createReduxHistoryContext({
+    history: createBrowserHistory(),
+});
 
 export const store = configureStore({
     reducer: {},
 });
 
+export const history = createReduxHistory(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
