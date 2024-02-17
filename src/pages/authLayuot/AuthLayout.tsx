a389de305fccab2d1b3c4fd772438f7eaebf07ef
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-17 15:20:17 +0300

    feat: add bg blur

--- src/pages/authLayuot/AuthLayout.tsx
+++ src/pages/authLayuot/AuthLayout.tsx
@@ -1,11 +1,18 @@
 import { Layout } from 'antd';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import bg from '../../assets/enter_page_light.png';
+import styles from './authLayout.module.scss';
 
 const AuthLayout: FC = () => {
     return (
-        <Layout>
-            <Outlet />
+        <Layout
+            className={styles.layout}
+            style={{ background: `no-repeat center bottom url(${bg})`, backgroundSize: 'cover' }}
+        >
+            <div className={styles.blurWrapper}>
+                <Outlet />
+            </div>
         </Layout>
     );
 };
