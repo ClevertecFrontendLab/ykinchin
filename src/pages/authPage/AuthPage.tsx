eaa7fc21b2047cb2cad309cec9c61dd228d2b8ad
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-18 22:10:31 +0300

    feat: add Tabs component

--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -1,13 +1,50 @@
 import IconWrapper from '@components/icons/IconWrapper';
 import FullLogo from '@components/icons/svgs/FullLogo';
+import { PATHS } from '@constants/PATHS';
+import { history } from '@redux/configure-store';
+import { Tabs } from 'antd';
 import { FC } from 'react';
 import styles from './authPage.module.scss';
+import SignInPage from './signIn/SignInPage';
+import SignUpPage from './signUp/SignUpPage';
 
+const AuthPage: FC = () => {
+    const handleChange = (key: string) => {
+        if (key === '1') {
+            history.push(PATHS.auth);
+        } else if (key === '2') {
+            history.push(PATHS.registration);
+        }
+    };
     return (
         <div className={styles.formWrapper}>
             <div className={styles.formWithLogo}>
                 <IconWrapper icon={FullLogo} style={{ width: 309 }} />
                 <div className={styles.formTabs}>
+                    <Tabs
+                        size='large'
+                        centered
+                        defaultActiveKey='1'
+                        onChange={handleChange}
+                        tabBarStyle={{
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            marginBottom: '14px',
+                        }}
+                        items={[
+                            {
+                                label: 'Вход',
+                                key: '1',
+                                children: <SignInPage />,
+                            },
+                            {
+                                label: 'Регистрация',
+                                key: '2',
+                                children: <SignUpPage />,
+                            },
+                        ]}
+                    />
                 </div>
             </div>
         </div>
