b1b0af39714abc937aa7ae2815964904b98896bf
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-18 22:06:32 +0300

    feat: remove depricated components

--- src/pages/signIn/SignInPage.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const signInPage: FC = () => {
-    return <div>signInPage</div>;
-};
-
-export default signInPage;
--- src/pages/signUp/SignUpPage.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const SignUpPage: FC = () => {
-    return <div>signUpPage</div>;
-};
-
-export default SignUpPage;
