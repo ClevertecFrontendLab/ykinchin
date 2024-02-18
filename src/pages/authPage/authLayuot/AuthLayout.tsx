3b3c11a768635f31e686706418a56ed9533eb086
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-18 22:08:23 +0300

    refactor: change folder structure

--- src/pages/authPage/authLayuot/AuthLayout.tsx
+++ src/pages/authPage/authLayuot/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import { Layout } from 'antd';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import bg from '../../assets/enter_page_light.png';
+import bg from '../../../assets/enter_page_light.png';
 import styles from './authLayout.module.scss';
 
 const AuthLayout: FC = () => {
